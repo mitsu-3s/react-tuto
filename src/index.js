c7eb744c7efeb38ee3d3a2d79254169a8c5fa06c
mitsu-3s <mitsuuu.s3s@gmail.com>
2023-03-02 14:06:34 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -42,6 +42,9 @@ class Board extends React.Component {
     handleClick(i) {
         // squaresの配列のコピーを作る
         const squares = this.state.squares.slice()
+        if (calculateWinner(squares) || squares[i]) {
+            return
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O' // xIsNextがTrueなら'X'、Falseなら'O'
         // ↑の変更が反映されたsqueresで上書きする
         this.setState({ squares: squares, xIsNext: !this.state.xIsNext })
@@ -53,7 +56,13 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
+        const winner = calculateWinner(this.state.squares)
+        let status
+        if (winner) {
+            status = 'Winner: ' + winner
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
+        }
 
         return (
             <div>
@@ -98,3 +107,23 @@ class Game extends React.Component {
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<Game />)
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i]
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a]
+        }
+    }
+    return null
+}
